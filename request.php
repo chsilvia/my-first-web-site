ab8c98b147217db9195551c49ac3c510b7687152
chsilvia <104757009+chsilvia@users.noreply.github.com>
2022-05-03 10:09:52 +0900

    Add files via upload

--- request.php
+++ request.php
@@ -1,6 +1,4 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-
-echo .$name."님의 이메일 주소는 ".$email."입니다.";
+echo "<p>name : ".$_GET['name']."</P>";
+echo "<p>email : ".$_GET['email']."</P>";
 ?>
